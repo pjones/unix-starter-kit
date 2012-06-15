0d004384cda53a22c6aff293b677914371cf39b7
Peter Jones <pjones@pmade.com>
2012-06-15 16:46:45 -0600

    Turn off ido-mode, it's too hard for new users, move auto-save files out of the current directory

--- emacs/lib/backups.el
+++ emacs/lib/backups.el
@@ -1,5 +1,6 @@
 ;;; backups.el --- Settings for Emacs backup files.
 (setq backup-directory-alist `(("." . ,(concat user-emacs-directory "backups")))
+      auto-save-file-name-transforms `(("." ,(concat user-emacs-directory "backups") t))
       delete-old-versions t
       kept-new-versions 6
       kept-old-versions 2
--- emacs/lib/completion.el
+++ emacs/lib/completion.el
@@ -1,15 +1,17 @@
 ;;; completion.el --- Auto-completion settings
 
 ;; ido-mode
-(ido-mode t)
-(setq ido-enable-prefix nil
-      ido-enable-flex-matching t
-      ido-auto-merge-work-directories-length nil
-      ido-create-new-buffer 'always
-      ido-use-filename-at-point 'guess
-      ido-use-virtual-buffers t
-      ido-handle-duplicate-virtual-buffers 2
-      ido-max-prospects 10)
+;; Don't use ido right now, it confuses new users and breaks C-x C-f
+;; from the menu bar.
+;; (ido-mode t)
+;; (setq ido-enable-prefix nil
+;;       ido-enable-flex-matching t
+;;       ido-auto-merge-work-directories-length nil
+;;       ido-create-new-buffer 'always
+;;       ido-use-filename-at-point 'guess
+;;       ido-use-virtual-buffers t
+;;       ido-handle-duplicate-virtual-buffers 2
+;;       ido-max-prospects 10)
 
 ;; Save your place in files you edit
 (require 'saveplace)
